04ef9f5730ed21b6c4d455757180e049509a0126
Daniel Palenzuela Álvarez <Daniel-link@hotmail.com>
2025-03-26 14:45:20 +0000

    Update main.cpp

--- pract4/main.cpp
+++ pract4/main.cpp
@@ -1,197 +1,219 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-#include "HashFunctions.hpp"
-#include "HashTable.hpp"
+#include "HashFunctions.hpp"  // Incluye la definición de persona y las funciones de dispersión/exploración
+#include "HashTable.hpp"      // Incluye las secuencias y la tabla hash
 
 using namespace std;
 
-// Daniel Palenzuela Álvarez alu0101140469
-// Ejemplos 
-// ./hash_program -ts 100 -fd 1 -hash open
-// ./hash_program -ts 150 -fd 2 -hash close -bs 3 -fe 2
-// ./hash_program -ts 200 -fd 3 -hash close -bs 4 -fe 3
-// ./hash_program -ts 50 -fd 2 -hash open
-
-
-// Función para mostrar el uso correcto del programa por línea de comandos.
+// Función que muestra información detallada sobre el uso del programa.
 void printUsage(char* progName) {
+    cout << "========================================\n";
     cout << "  Programa de Tabla Hash - Búsqueda por Dispersión\n";
-    cout << "Instrucciones:\n";
+    cout << "========================================\n\n";
+    cout << "Descripción:\n";
+    cout << "  Este programa implementa una tabla hash configurable para almacenar\n";
+    cout << "  registros de personas. Cada persona tiene un ID con formato:\n";
+    cout << "    alu<7 dígitos>, prof<7 dígitos> o pas<7 dígitos>\n";
+    cout << "  y además guarda su nombre, primer apellido y segundo apellido.\n\n";
+    cout << "Uso:\n";
     cout << "  " << progName << " -ts <tableSize> -fd <fdCode> -hash <open|close> [-bs <blockSize>] [-fe <feCode>]\n\n";
     cout << "Opciones:\n";
-    cout << "  -ts <tableSize>   Tamaño de la tabla hash (número de celdas).\n";
-    cout << "  -fd <fdCode>      Función de dispersión:\n";
-    cout << "                      1  -> Módulo\n";
-    cout << "                      2  -> Suma de dígitos\n";
-    cout << "                      3  -> Pseudoaleatoria\n";
+    cout << "  -ts <tableSize>     Número de celdas de la tabla hash.\n";
+    cout << "  -fd <fdCode>        Código de la función de dispersión:\n";
+    cout << "                         1  -> Módulo (h(k) = valor_numerico % tableSize)\n";
+    cout << "                         2  -> Suma de dígitos (h(k) = suma(dígitos) % tableSize)\n";
+    cout << "                         3  -> Pseudoaleatoria (h(k) = {srand(valor_numerico); rand()} % tableSize)\n";
     cout << "  -hash <open|close>  Tipo de dispersión:\n";
-    cout << "                      open  -> Dispersión abierta (usa listas dinámicas).\n";
-    cout << "                      close -> Dispersión cerrada (usa arrays estáticos).\n";
-    cout << "  -bs <blockSize>   Tamaño del bloque (exclusivo de close, número máximo de registros por celda).\n";
-    cout << "  -fe <feCode>      Función de exploración (exclusivo de close):\n";
-    cout << "                      1  -> Exploración lineal\n";
-    cout << "                      2  -> Exploración cuadrática\n";
-    cout << "                      3  -> Doble dispersión\n";
-    cout << "                      4  -> Redispersión\n\n";
+    cout << "                         open  -> Dispersión abierta (usa listas dinámicas).\n";
+    cout << "                         close -> Dispersión cerrada (usa arrays estáticos).\n";
+    cout << "  -bs <blockSize>     Tamaño máximo de registros por celda (solo para 'close').\n";
+    cout << "  -fe <feCode>        Código de la función de exploración (solo para 'close'):\n";
+    cout << "                         1  -> Exploración lineal (g(k,i) = i)\n";
+    cout << "                         2  -> Exploración cuadrática (g(k,i) = i^2)\n";
+    cout << "                         3  -> Doble dispersión (g(k,i) = f(k) * i)\n";
+    cout << "                         4  -> Redispersión (g(k,i) = f(i)(k))\n\n";
+    cout << "Ejemplos:\n";
+    cout << "  Dispersión cerrada con exploración lineal:\n";
+    cout << "    " << progName << " -ts 100 -fd 1 -hash close -bs 5 -fe 1\n";
+    cout << "  Dispersión abierta usando función de suma de dígitos:\n";
+    cout << "    " << progName << " -ts 50 -fd 2 -hash open\n";
+    cout << "  Para ver esta ayuda:\n";
+    cout << "    " << progName << " --help\n";
+    cout << "========================================\n";
 }
 
 int main(int argc, char* argv[]) {
-    // Usando --help se muestra el mensaje de ayuda y se sale.
+    // Muestra ayuda si se usa '--help'
     if(argc == 2 && (strcmp(argv[1], "--help") == 0)) {
         printUsage(argv[0]);
         return 0;
     }
-
+    
     // Verifica que se hayan pasado al menos los parámetros mínimos.
     if(argc < 5) {
         printUsage(argv[0]);
         return 1;
     }
 
-    // Variables para almacenar los parámetros de la línea de comandos.
-    unsigned tableSize = 0;  // Tamaño de la tabla hash
-    unsigned blockSize = 0;  // Tamaño de bloque para dispersión cerrada
-    int fdCode = 0;          // Código para elegir la función de dispersión
-    int feCode = 0;          // Código para elegir la función de exploración (dispersión cerrada)
-    string hashType = "";    // Tipo de dispersión open o close
+    // Variables para los parámetros.
+    unsigned tableSize = 0;
+    unsigned blockSize = 0;
+    int fdCode = 0;
+    int feCode = 0;
+    string hashType = "";
     
     // Procesa los argumentos de línea de comandos.
     for(int i = 1; i < argc; i++){
         if(strcmp(argv[i], "-ts") == 0 && i + 1 < argc) {
-            tableSize = atoi(argv[++i]); // Asigna el tamaño de la tabla
+            tableSize = atoi(argv[++i]);
         } else if(strcmp(argv[i], "-fd") == 0 && i + 1 < argc) {
-            fdCode = atoi(argv[++i]);    // Asigna el código de función de dispersión
+            fdCode = atoi(argv[++i]);
         } else if(strcmp(argv[i], "-hash") == 0 && i + 1 < argc) {
-            hashType = argv[++i];         // Asigna el tipo de dispersión (open o close)
+            hashType = argv[++i];
         } else if(strcmp(argv[i], "-bs") == 0 && i + 1 < argc) {
-            blockSize = atoi(argv[++i]);  // Asigna el tamaño de bloque (solo para dispersión cerrada)
+            blockSize = atoi(argv[++i]);
         } else if(strcmp(argv[i], "-fe") == 0 && i + 1 < argc) {
-            feCode = atoi(argv[++i]);     // Asigna el código de función de exploración (solo para dispersión cerrada)
+            feCode = atoi(argv[++i]);
         }
     }
     
-    // Verifica que se hayan establecido los parámetros mínimos requeridos.
     if(tableSize == 0 || fdCode == 0 || hashType == "") {
         printUsage(argv[0]);
         return 1;
     }
     
-    // Inicializa la semilla para números aleatorios.
     srand(time(NULL));
     
-    // Crear la función de dispersión según el código especificado.
-    DispersionFunction<nif>* df = nullptr;
+    // Crea la función de dispersión según el código.
+    DispersionFunction<persona>* df = nullptr;
     switch(fdCode) {
         case 1:
-            df = new ModuleHashFunction<nif>(tableSize);
+            df = new ModuleHashFunction<persona>(tableSize);
             break;
         case 2:
-            df = new SumHashFunction<nif>(tableSize);
+            df = new SumHashFunction<persona>(tableSize);
             break;
         case 3:
-            df = new PseudoRandomHashFunction<nif>(tableSize);
+            df = new PseudoRandomHashFunction<persona>(tableSize);
             break;
         default:
             cout << "Código de función de dispersión inválido." << endl;
             return 1;
     }
     
-    // Si se selecciona dispersión abierta, se utiliza dynamicSequence.
+    // Si se usa dispersión abierta.
     if(hashType == "open") {
-        // Crea la tabla hash con dynamicSequence. No se usa blockSize ni exploración.
-        HashTable<nif, dynamicSequence<nif> > table(tableSize, *df);
+        HashTable<persona, dynamicSequence<persona> > table(tableSize, *df);
         
         int option;
-        nif key;
-        // Menú interactivo para añadir y buscar claves.
+        persona p;
         do {
-            cout << "\nMenú principal:\n1. Insertar\n2. Buscar\n0. Salir\nOpción: ";
+            cout << "\nMenú:\n1. Insertar\n2. Buscar\n0. Salir\nOpción: ";
             cin >> option;
             if(option == 1) {
-                long num;
-                cout << "Introduce el número de nif (8 dígitos): ";
-                cin >> num;
-                key = nif(num);
-                if(table.insert(key))
-                    cout << "Añadido correctamente." << endl;
+                // Se solicita introducir los datos de la persona.
+                std::string id, nombre, ape1, ape2;
+                cout << "Introduce el ID (formato alu/prof/pas seguido de 7 dígitos): ";
+                cin >> id;
+                cout << "Introduce el nombre: ";
+                cin >> nombre;
+                cout << "Introduce el primer apellido: ";
+                cin >> ape1;
+                cout << "Introduce el segundo apellido: ";
+                cin >> ape2;
+                p = persona(id, nombre, ape1, ape2);
+                if(table.insert(p))
+                    cout << "Insertado correctamente." << endl;
                 else
                     cout << "Error al insertar." << endl;
             } else if(option == 2) {
-                long num;
-                cout << "Introduce el número de nif (8 dígitos): ";
-                cin >> num;
-                key = nif(num);
-                if(table.search(key))
+                std::string id, nombre, ape1, ape2;
+                cout << "Introduce el ID (formato alu/prof/pas seguido de 7 dígitos): ";
+                cin >> id;
+                cout << "Introduce el nombre: ";
+                cin >> nombre;
+                cout << "Introduce el primer apellido: ";
+                cin >> ape1;
+                cout << "Introduce el segundo apellido: ";
+                cin >> ape2;
+                p = persona(id, nombre, ape1, ape2);
+                if(table.search(p))
                     cout << "Encontrado." << endl;
                 else
                     cout << "No encontrado." << endl;
             }
         } while(option != 0);
-    } 
-    // Si se selecciona dispersión cerrada, se utiliza staticSequence.
+    }
+    // Si se usa dispersión cerrada.
     else if(hashType == "close") {
-        // Verifica que se hayan especificado blockSize y código de función de exploración.
         if(blockSize == 0 || feCode == 0) {
             cout << "Para dispersión cerrada se deben proporcionar blockSize y código de función de exploración." << endl;
             return 1;
         }
-        ExplorationFunction<nif>* ef = nullptr;
-        // Selecciona la función de exploración según el código.
+        ExplorationFunction<persona>* ef = nullptr;
         switch(feCode) {
             case 1:
-                ef = new LinearExploration<nif>();
+                ef = new LinearExploration<persona>();
                 break;
             case 2:
-                ef = new QuadraticExploration<nif>();
+                ef = new QuadraticExploration<persona>();
                 break;
             case 3:
-                ef = new DoubleHashExploration<nif>(*df);
+                ef = new DoubleHashExploration<persona>(*df);
                 break;
             case 4:
-                ef = new RedispersionExploration<nif>();
+                ef = new RedispersionExploration<persona>();
                 break;
             default:
                 cout << "Código de función de exploración inválido." << endl;
                 return 1;
         }
-        // Crea la tabla hash con staticSequence, utilizando blockSize y la función de exploración.
-        HashTable<nif, staticSequence<nif> > table(tableSize, *df, *ef, blockSize);
+        HashTable<persona, staticSequence<persona> > table(tableSize, *df, *ef, blockSize);
         
         int option;
-        nif key;
-        // Menú interactivo para insertar y buscar claves.
+        persona p;
         do {
-            cout << "\nMenú principal:\n1. Insertar\n2. Buscar\n0. Salir\nOpción: ";
+            cout << "\nMenú:\n1. Insertar\n2. Buscar\n0. Salir\nOpción: ";
             cin >> option;
             if(option == 1) {
-                long num;
-                cout << "Introduce el número de nif (8 dígitos): ";
-                cin >> num;
-                key = nif(num);
-                if(table.insert(key))
-                    cout << "Añadido correctamente." << endl;
+                std::string id, nombre, ape1, ape2;
+                cout << "Introduce el ID (formato alu/prof/pas seguido de 7 dígitos): ";
+                cin >> id;
+                cout << "Introduce el nombre: ";
+                cin >> nombre;
+                cout << "Introduce el primer apellido: ";
+                cin >> ape1;
+                cout << "Introduce el segundo apellido: ";
+                cin >> ape2;
+                p = persona(id, nombre, ape1, ape2);
+                if(table.insert(p))
+                    cout << "Insertado correctamente." << endl;
                 else
-                    cout << "Error al insertar (posible saturación en el bloque o tabla)." << endl;
+                    cout << "Error al insertar (posible saturación en la celda o tabla)." << endl;
             } else if(option == 2) {
-                long num;
-                cout << "Introduce el número de nif (8 dígitos): ";
-                cin >> num;
-                key = nif(num);
-                if(table.search(key))
+                std::string id, nombre, ape1, ape2;
+                cout << "Introduce el ID (formato alu/prof/pas seguido de 7 dígitos): ";
+                cin >> id;
+                cout << "Introduce el nombre: ";
+                cin >> nombre;
+                cout << "Introduce el primer apellido: ";
+                cin >> ape1;
+                cout << "Introduce el segundo apellido: ";
+                cin >> ape2;
+                p = persona(id, nombre, ape1, ape2);
+                if(table.search(p))
                     cout << "Encontrado." << endl;
                 else
                     cout << "No encontrado." << endl;
             }
         } while(option != 0);
-        // Libera la memoria asignada a la función de exploración.
         delete ef;
     } else {
-        cout << "Tipo de hash inválido. Usa open o close." << endl;
+        cout << "Tipo de hash inválido. Usa 'open' o 'close'." << endl;
         return 1;
     }
     
-    // Libera la memoria asignada a la función de dispersión.
     delete df;
     return 0;
 }
